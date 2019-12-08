a889e1f38ac11ef778091a9142a78bf2972b60fa
fdss-song <37619372+fdss-song@users.noreply.github.com>
2019-12-08 21:05:51 +0800

    修改了部分不合适的地方
    修改rtt为EstimatedRTT，意思更加明确；增加bool、alpha"、beta的宏定义；删去了window_t；

--- inc/global.h
+++ inc/global.h
@@ -14,56 +14,16 @@
 #define NO_WAIT 1
 #define TIMEOUT 2
 
+#define bool int 
+/* C99: #define bool _Bool */
 #define TRUE 1
 #define FALSE 0
 
+#define alpha 0.125
+#define beta 0.25
+
 #define SEQMAX 0xffffffff
 
-typedef struct {
-	uint32_t last_seq_received; /* 与原来表示内容不同，此处相当于接收方的窗口base，表示当前希望收到的序列号 */
-	uint32_t last_ack_received; /* 最近一次被确认的序列号，相当于发送方的窗口base */
-	pthread_mutex_t ack_lock; /* 单线程的话不需要 */
-	sent_pkt *sent_head; /* 带头结点，指的是如果链表为空，head->next=NULL */
-	uint32_t sent_length; /* 发送但未被确认的数据字节数，nextseqnum = sent_length + last_ack_received；
-							 sent_length 可能会大于 window_size */
-	recv_pkt *recv_head;
-	uint32_t recv_length; /* 缓存的数据长度 */
-	pthread_mutex_t recv_lock; /* TCP收到数据，放进去；cmu_read读数据 */
-	uint32_t rwnd; /* 流量控制 */
-	uint32_t cwnd; /* 拥塞控制 */
-	uint32_t rtt; /* RTT */
-    uint32_t DevRTT; /* DevRTT ms*/
-	uint32_t ssthresh; /* ssthresh */
-	bool timer_on; /* 计时器是否设置 */
-    control_status con_state;/* 用于拥塞控制算法表示状态 */
-    uint32_t TimeoutInterval;/* TimeoutInterval ms*/
-}slide_window_t;
-
-/* 每一个包的起始地址和发送时间 */
-typedef struct {
-	char *pkt_start; /* 包括头部 */
-	struct timeval sent_time; /* 用来计算RTT，单指第一次发送的时间 */
-    bool is_resend; /* 是否重发 */
-	sent_pkt *next;
-}sent_pkt;
-
-/* 缓存乱序到达的包的数据，此链表按照seq排序 */
-typedef struct {
-	char *data_start; /* 只有数据 */
-	uint32_t seq; /* 数据的序列号 */
-	uint32_t data_length; /* 数据长度 */
-	bool adjacent; /* 表示与前面的数据包是否相邻，相邻为true
-					（如果当前接收方正在等待的包，也为true）；
-					考虑到维护的是有序链表，减少计算次数 */
-	/* 因此cmu_read是从第一个true一直读到第一个false */
-	recv_pkt *next;
-}recv_pkt;
-
-typedef struct {
-	uint32_t last_seq_received;
-	uint32_t last_ack_received;
-	pthread_mutex_t ack_lock;
-} window_t;
 
 typedef enum {
 	CLOSED, /* 0 */
@@ -77,7 +37,7 @@ typedef enum {
 	TIME_WAIT, /* 8 */
 	CLOSE_WAIT, /* 9 */
 	LAST_ACK /* 10 */
-}states;
+}link_states;
 
 typedef enum {
     SLOW_STAR, /* 0 */
@@ -85,22 +45,61 @@ typedef enum {
     FAST_RECO  /* 2 */
 }control_status;
 
+/* 每一个包的起始地址和发送时间 */
+typedef struct {
+	char *pkt_start; /* 包括头部 */
+	bool is_resend;
+	struct timeval sent_time; /* 用来计算RTT，单指第一次发送的时间 */
+	struct sent_pkt *next;
+}sent_pkt;
+
+/* 缓存乱序到达的包的数据，此链表按照seq排序 */
+typedef struct {
+	char *data_start; /* 只有数据 */
+	uint32_t seq; /* 数据的序列号 */
+	uint32_t data_length; /* 数据长度 */
+	bool adjacent; /* 表示与前面的数据包是否相邻，相邻为true
+					（如果当前接收方正在等待的包，也为true）；
+					考虑到维护的是有序链表，减少计算次数 */
+	/* 因此cmu_read是从第一个true一直读到第一个false */
+	struct recv_pkt *next;
+}recv_pkt;
+
+typedef struct {
+	uint32_t last_seq_received; /* 与原来表示内容不同，此处相当于接收方的窗口base，表示当前希望收到的序列号 */
+	uint32_t last_ack_received; /* 最近一次被确认的序列号，相当于发送方的窗口base */
+	pthread_mutex_t ack_lock; /* 单线程的话不需要 */
+	sent_pkt *sent_head; /* 带头结点，指的是如果链表为空，head->next=NULL */
+	uint32_t sent_length; /* 发送但未被确认的数据字节数，nextseqnum = sent_length + last_ack_received；
+							 sent_length 可能会大于 window_size */
+	recv_pkt *recv_head;
+	uint32_t recv_length; /* 缓存的数据长度 */
+	pthread_mutex_t recv_lock; /* TCP收到数据，放进去；cmu_read读数据 */
+	uint32_t rwnd; /* 流量控制 */
+	uint32_t cwnd; /* 拥塞控制 */
+	uint32_t EstimatedRTT;
+	uint32_t DevRTT;
+	uint32_t ssthresh; /* ssthresh */
+	bool timer_on; /* 计时器是否设置 */
+	control_status con_state;/* 用于拥塞控制算法表示状态 */
+	uint32_t TimeoutInterval;
+}slide_window_t;
+
 typedef struct {
 	int socket;   
 	pthread_t thread_id;
 	uint16_t my_port;
 	uint16_t their_port;
 	struct sockaddr_in conn;
-	pthread_mutex_t recv_lock;
 	pthread_cond_t wait_cond;
 	char* sending_buf;
 	int sending_len;
 	int type;
 	pthread_mutex_t send_lock;
-	int dying;
+	bool dying; /* 1表示结束；0表示未结束 */
 	pthread_mutex_t death_lock;
 	slide_window_t window;
-	enum states state;
+	link_states state;
 	uint32_t ISN;
 	uint32_t FSN;
     int ack_dup;/* ack重复的数目 用于切换到快速重传 */
