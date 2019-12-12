0c673203e6d9be33a568696a14078ada111f81ec
fdss-song <37619372+fdss-song@users.noreply.github.com>
2019-12-12 21:52:34 +0800

    在原始基础上稍微改变

--- src/client.c
+++ src/client.c
@@ -12,15 +12,30 @@ void functionality(cmu_socket_t  * sock){
     int read;
     FILE *fp;
 
-    fp = fopen("./test/20M.txt", "rb");
+    cmu_write(sock, "hi there", 9);
+    cmu_write(sock, "hi there2", 10);
+    cmu_write(sock, "hi there3", 10);
+    cmu_write(sock, "hi there4", 10);
+    cmu_write(sock, "hi there5", 10);
+    cmu_write(sock, "hi there6", 10);
+    cmu_read(sock, buf, 200, NO_FLAG);
+
+    cmu_write(sock, "hi there", 9);
+    cmu_read(sock, buf, 200, NO_FLAG);
+    printf("R: %s\n", buf);
+
+    read = cmu_read(sock, buf, 200, NO_WAIT);
+    printf("Read: %d\n", read);
+
+    // fp = fopen("./src/cmu_tcp.c", "rb");
+    fp = fopen("./test/600K.file", "rb");
+    
     read = 1;
-    while(TRUE){
-        while(read > 0 ) {
-            read = fread(buf, 1, 9898, fp);
-            if (read > 0) {
-                cmu_write(sock, buf, read);
-            }
-        }
+    while(read > 0 ){
+        read = fread(buf, 1, 9898, fp);
+        // sleep(3);
+        if(read > 0)
+            cmu_write(sock, buf, read);
     }
 }
 
@@ -33,7 +48,7 @@ void functionality(cmu_socket_t  * sock){
  *
  */
 int main(int argc, char **argv) {
-	int portno;
+    int portno;
     char *serverip;
     char *serverport;
     cmu_socket_t socket;
